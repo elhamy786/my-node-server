ff185a951290f13e85c136843eb5c884d070aa7e
elhamy786 <elham.afzali1383@gmail.com>
2024-10-26 21:51:17 +0430

    Edit package.json file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const galaxiesRoute = require("./routes/galaxies");
+const galaxiesRoute = require("./api/galaxies");
 
 const app = express();
 const PORT = 3000;
