f69b20324cb7fcad853a8f6f3fb45bb25017c4f0
elhamy786 <elham.afzali1383@gmail.com>
2024-10-26 21:31:13 +0430

    Edit package.json file

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
 const galaxiesRoute = require("./routes/galaxies");
 
 const app = express();
-const PORT = process.env.PORT || 3000; // Use environment variable for Vercel
+const PORT = 3000;
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "public"))); // Serve static files from the public directory
+app.use(express.static("public"));
+app.use("/galaxies", galaxiesRoute);
 
-// Set up your routes
-app.use("/api/galaxies", galaxiesRoute);
-
-// Fallback route to serve index.html for all other requests
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-// Start the server
 app.listen(PORT, () => {
   console.log(`Galaxy API server is running on http://localhost:${PORT}`);
 });
